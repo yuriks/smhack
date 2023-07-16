ccb35c38390a1ec0aec68f6e4072bce31ea3f53e
Yuri Kunde Schlesner <yuriks@yuriks.net>
2023-07-16 04:33:34 -0700

    INCBIN extended pausemap graphics
    Also adds several new map tiles.
    
    These were being managed by just expanding the SMART-managed
    pausemap.gfx file, but TestRunner said it's not explicitly supported, so
    include them from asm instead.

--- project/ASM/map_tile_repoints.asm
+++ project/ASM/map_tile_repoints.asm
@@ -5,6 +5,12 @@ org $81B14B
 	; Replaces bottom part of pause tilemap in the post-hexmap screen
 	incbin "gfx/hexmap_controls_tilemap.tbl"
 
+org $B6E000
+	; Extended pause screen graphics. SMART seems able to somewhat work
+	; just directly extending the pausemap.gfx file, but this is not
+	; supported, so to be safe it's being included via asm.
+	incbin "gfx/pausemap_extended.gfx"
+
 org $E08000
 	; Originally at B6E000
 	; Address used in MapOverhaul asm
